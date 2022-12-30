21497b412d784fe5cde3eb77155b8d8a1e734dc5
hilmi razib yusuf <hilmi.inolabs@gmail.com>
2022-12-30 15:44:37 +0700

    api main banner

--- kiriminaja/pages/index.vue
+++ kiriminaja/pages/index.vue
@@ -1,60 +1,15 @@
 <template>
   <div class="area-kiriminaja bg-kiriminaja h-100 w-100 d-block text-white">
+    <client-only>
+      <carousel :loop="true" :autoplay="true" ::items="1" :center="true" :stagePadding="10"
+        :margin="50">
+        <div v-for="main in mainBanner" :key="main.id">
+          <Banner :title="main.title" :desc="main.description" :image="main.image" :link="main.href"/>
+        </div>
+      </carousel>
+    </client-only>
     <section class="position-relative py-3">
       <div class="container">
-        <div class="row mb-5">
-          <div class="col-lg-7 col-12 order-lg-1 order-2">
-            <div class="mb-5">
-              <h1 class="fw-bold text-h1 mb-4 pe-lg-4">
-                Borzo Delivery Kini Sudah Hadir di KiriminAja!
-              </h1>
-              <p class="w-75 pe-lg-4 text-p-header" style="color: #aeaaae">
-                Kini pengiriman instant di KiriminAja makin banyak pilihannya!
-                Update aplikasi KiriminAja-mu dan cobain kirim paket instant
-                pakai Borzo Delivery! Pengiriman Instant
-              </p>
-            </div>
-            <ul class="list-group gap-3 mb-5">
-              <li class="">
-                <ion-icon name="shield-checkmark-outline"></ion-icon>
-                <span>Pengiriman Instant</span>
-              </li>
-              <li class="">
-                <ion-icon name="shield-checkmark-outline"></ion-icon>
-                <span>Mulai dari Rp 2.000/km</span>
-              </li>
-              <li class="">
-                <ion-icon name="shield-checkmark-outline"></ion-icon>
-                <span>Bisa diakses di seluruh wilayah Pulau Jawa, Bali, Balikpapan,
-                  Banda Aceh, dan Bandar Lampung.</span>
-              </li>
-            </ul>
-            <div class="d-lg-flex d-block gap-3">
-              <a class="nav-link active d-block text-white py-2 px-3 btn-signup" aria-current="page" href="#">
-                <div class="d-flex justify-content-center align-items-center gap-2">
-                  <span>Daftar Gratis</span>
-                  <svg width="20" height="20" viewBox="0 0 512 512">
-                    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                      stroke-width="40" d="m268 112l144 144l-144 144m124-144H100" />
-                  </svg>
-                </div>
-              </a>
-              <a href="#" class="nav-link active text-white">
-                <div class="d-flex justify-content-center align-items-center gap-2">
-                  <svg width="20" height="20" viewBox="0 0 512 512">
-                    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                      stroke-width="40"
-                      d="M256 160c16-63.16 76.43-95.41 208-96a15.94 15.94 0 0 1 16 16v288a16 16 0 0 1-16 16c-128 0-177.45 25.81-208 64c-30.37-38-80-64-208-64c-9.88 0-16-8.05-16-17.93V80a15.94 15.94 0 0 1 16-16c131.57.59 192 32.84 208 96Zm0 0v288" />
-                  </svg>
-                  <span>Selengkapnya</span>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div class="col-lg-5 col-12 order-lg-2 order-1">
-            <img :src="require('@/assets/slider-1.webp')" class="img-fluid" alt="" />
-          </div>
-        </div>
         <div class="row">
           <div class="col-12">
             <form class="form-cek-ongkir">
@@ -415,13 +370,19 @@
 </template>
 
 <script>
+import Banner from '~/components/Banner.vue';
 export default {
   name: "IndexPage",
+  components: {
+    Banner
+  },
   data: () => ({
     dataBanner: [],
+    mainBanner: []
   }),
   async fetch() {
     this.dataBanner = await fetch("http://localhost:3000/Api/Banner.json").then((res) => res.json());
+    this.mainBanner = await fetch("http://localhost:3000/Api/MainBanner.json").then((res) => res.json());
   },
 };
 </script>
